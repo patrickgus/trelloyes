303c11fb74805e05c385e697d96bb464ef667f76
Patrick Gustafson <patrickgus@charter.net>
2019-11-19 20:43:07 -0800

    export Card

--- src/composition/Card.js
+++ src/composition/Card.js
@@ -1,5 +1,13 @@
 import React from 'react';
+import { exportSpecifier } from '@babel/types';
 
 function Card(props) {
-  
+  return (
+    <div className="Card">
+      <h3>{props.title}</h3>
+      <p>{props.content}</p>
+    </div>
+  );
 }
+
+export default Card;
