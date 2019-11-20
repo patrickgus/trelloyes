729a6a92a2ac0de536dad67d2ad00f0fa4190b86
Patrick Gustafson <patrickgus@charter.net>
2019-11-19 21:13:04 -0800

    add content to app

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import './index.css';
-import STORE from './store';
+import STORE from './STORE'
+import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <App store={STORE} />,
+  document.getElementById('root'),
+);
