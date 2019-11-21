f2abe4b520ecd4e3027cea58becc32278515c5c3
Patrick Gustafson <patrickgus@charter.net>
2019-11-21 13:52:11 -0800

    add snapshot test for List

--- src/List.test.js
+++ src/List.test.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import List from './List';
+import renderer from 'react-test-renderer';
 
 describe('List component', () => {
   it('renders without crashing', () => {
     const section = document.createElement('section');
     ReactDOM.render(<List />, section);
     ReactDOM.unmountComponentAtNode(section);
+  });
+
+  it('renders the UI as expected', () => {
+    const tree = renderer
+      .create(<List header="First list" />)
+      .toJSON();
+    expect(tree).toMatchSnapshot();  
   });
 });
