e5004ca355fa1d8abe5f1e831bdfc248d420b6e4
Patrick Gustafson <patrickgus@charter.net>
2020-01-07 20:18:11 -0800

    create snapshot test for list

--- src/List.js
+++ src/List.js
@@ -9,12 +9,15 @@ function List(props) {
         <h2>{props.header}</h2>
       </header>
       <div className='List-cards'>
-        {props.cards.map((card) =>
-          <Card
-            key={card.id}
-            title={card.title}
-            content={card.content}
-          />
+        {props.cards.map((card) => {
+          console.log(card);
+          return (
+            <Card
+              key={card.id}
+              title={card.title}
+              content={card.content}
+            />
+          )} 
         )}
         <button
           type='button'
--- src/List.test.js
+++ src/List.test.js
@@ -6,13 +6,22 @@ import renderer from 'react-test-renderer';
 describe('List component', () => {
   it('renders without crashing', () => {
     const section = document.createElement('section');
-    ReactDOM.render(<List />, section);
+    ReactDOM.render(<List header="" cards={[]} />, section);
     ReactDOM.unmountComponentAtNode(section);
   });
 
   it('renders the UI as expected', () => {
     const tree = renderer
-      .create(<List header="First list" />)
+      .create(<List header="First list"  cards={[
+        { id: 'a', title: 'First card', content: 'lorem ipsum' },
+        { id: 'b', title: 'Second card', content: 'lorem ipsum' },
+        { id: 'e', title: 'Fifth card', content: 'lorem ipsum' },
+        { id: 'f', title: 'Sixth card', content: 'lorem ipsum' },
+        { id: 'g', title: 'Seventh card', content: 'lorem ipsum' },
+        { id: 'j', title: 'Tenth card', content: 'lorem ipsum' },
+        { id: 'l', title: 'Twelfth card', content: 'lorem ipsum' },
+        { id: 'm', title: 'Thirteenth card', content: 'lorem ipsum' }
+      ]} />)
       .toJSON();
     expect(tree).toMatchSnapshot();  
   });
